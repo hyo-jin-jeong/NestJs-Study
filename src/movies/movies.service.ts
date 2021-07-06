60154d20360cd5fa5900e246bd49d39756a01b66
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-07-06 14:08:54 +0900

    UNIT TESTING: by jest

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -5,35 +5,35 @@ import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
-    private movies:Movie[] = [];
+    private movies: Movie[] = [];
 
-    getAll(): Movie[]{
+    getAll(): Movie[] {
         return this.movies;
     }
 
-    getOne(id: number): Movie{
-        const movie = this.movies.find(movie =>movie.id === id);
-        if(!movie){
+    getOne(id: number): Movie {
+        const movie = this.movies.find(movie => movie.id === id);
+        if (!movie) {
             throw new NotFoundException(`Movie with ID: ${id} not found.`)
         }
         return movie;
     }
 
-    deleteOne(id: number){
+    deleteOne(id: number) {
         this.getOne(id);
         this.movies = this.movies.filter(movie => movie.id !== +id);
     }
 
-    create(movieData: CreateMovieDto){
+    create(movieData: CreateMovieDto) {
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData
         })
     }
 
-    update(id: number, updateData: UpdateMovieDto){
+    update(id: number, updateData: UpdateMovieDto) {
         const movie = this.getOne(id);
         this.deleteOne(id);
-        this.movies.push({ ...movie, ...updateData})
+        this.movies.push({ ...movie, ...updateData })
     }
 }
