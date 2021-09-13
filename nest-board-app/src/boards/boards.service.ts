af2b2edddd626546323f512b63dbc4a90bb56a08
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-09-13 18:53:32 +0900

    Add: nest-board-app Custom Pipe 추가

--- nest-board-app/src/boards/boards.service.ts
+++ nest-board-app/src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -11,7 +11,12 @@ export class BoardsService {
     }
 
     getBoardById(id: string): Board {
-        return this.boards.find((board) => board.id === id)
+        const found =  this.boards.find((board) => board.id === id);
+        
+        if(!found){
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        return found
     }
 
     createBoard(createBoardDto: CreateBoardDto) {
@@ -29,7 +34,8 @@ export class BoardsService {
     }
 
     deleteBoard(id: string): void {
-        this.boards = this.boards.filter((board) => board.id !== id);
+        const found = this.getBoardById(id);
+        this.boards = this.boards.filter((board) => board.id !== found.id);
     }
 
     updateBoardStatus(id: string, status: BoardStatus): Board {
