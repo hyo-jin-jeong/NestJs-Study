8acc2763ebd7c95e12c6840b56ad52ea9e377cfe
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-09-13 17:45:42 +0900

    Create nest-board-app createBoard logic 생성

--- nest-board-app/src/boards/boards.controller.ts
+++ nest-board-app/src/boards/boards.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
 import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardService: BoardsService){}
+    constructor(private boardService: BoardsService) { }
 
     @Get('/')
-    getAllBoard(): Board[]{
+    getAllBoard(): Board[] {
         return this.boardService.getAllBoards();
+    }
+    @Post()
+    createBoard(
+        @Body('title') title: string, 
+        @Body('description') description: string
+    ): Board{
+        return this.boardService.createBoard(title, description);
     }
 }
